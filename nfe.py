f1ed18613d94d89fa624e338fb29361998c4a169
Tiago Guimarães <tilacog@gmail.com>
2017-07-31 14:09:54 -0300

    can download all schema packs

--- nfe.py
+++ nfe.py
@@ -1,7 +1,6 @@
 import pathlib
 import luigi
 import requests
-from luigi.util import requires
 from pyquery import PyQuery as pq
 
 
@@ -24,14 +23,27 @@ class FetchAvailableSchemaPacks(luigi.Task):
         return [f'{self.BASE_DOWNLOAD_URL}/{link}' for link in download_links]
 
 
-@requires(FetchAvailableSchemaPacks)
 class DownloadSchemaPacks(luigi.Task):
     'download all schema packs'
+
+    DEST_DIR = luigi.Parameter()
+
+    def requires(self):
+        return FetchAvailableSchemaPacks()
+
+    def output(self):
+        return luigi.LocalTarget('downloaded.txt')
+
     def run(self):
+        # get list of urls to download
         with self.input().open() as f:
             available_schema_packs = f.readlines()
-        # if exists, skip
-        # else, download
+        # download them all
+        # TODO: be idempotent (only download missing files)
+        downloaded = download_many(available_schema_packs)
+        # write list of downloaded files to a file
+        with self.output().open('w') as f2:
+            f2.writelines(i + '\n' for i in downloaded)
 
 
 class UpsertDatabase(luigi.Task):
@@ -42,7 +54,8 @@ class UpsertDatabase(luigi.Task):
 def download(url, dest_dir='downloaded'):
     pathlib.Path(dest_dir).mkdir(exist_ok=True)
     r = requests.get(url, stream=True)
-    local_filename = (pathlib.Path(dest_dir) / (url.split('/')[-1] + '.zip')).as_posix()
+    local_filename = (pathlib.Path(dest_dir) /
+                      (url.strip().split('/')[-1] + '.zip')).as_posix()
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
@@ -53,7 +66,3 @@ def download(url, dest_dir='downloaded'):
 def download_many(url_list):
     downloaded = list(map(download, url_list))
     return downloaded
-
-with open('urls.txt') as f:
-    data = f.read().splitlines()
-download_many(data)
