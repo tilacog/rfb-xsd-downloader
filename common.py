884447edabae792fc4bd4f9068009d3b7ecec169
Tiago Guimarães <tilacog@gmail.com>
2017-08-15 14:51:33 -0300

    can download esocial schema packs

--- common.py
+++ common.py
@@ -1,13 +1,16 @@
 import pathlib
 import requests
-from urlib.parse import urlparse
+from urllib.parse import urlparse
 
 
 def download(url, dest_dir='downloaded'):
-    pathlib.Path(dest_dir).mkdir(exist_ok=True)
-    r = requests.get(url, stream=True)
+    pathlib.Path(dest_dir).mkdir(exist_ok=True, parents=True)
+    r = requests.get(url, stream=True, verify=False)
+
     local_filename = (pathlib.Path(dest_dir) /
-                      (url.strip().split('/')[-1] + '.zip')).as_posix()
+                      (url.strip().split('/')[-1])).as_posix()
+    if not local_filename.endswith('.zip'):
+        local_filename += '.zip'
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
--- esocial.py
+++ esocial.py
@@ -1,6 +1,8 @@
+import datetime
 import luigi
+import yaml
 import requests
-from urllib.parse import urlparse
+from common import url_is_valid, download
 
 from pyquery import PyQuery as pq
 
@@ -25,8 +27,26 @@ class EsocialFetchAvailableSchemaPacks(luigi.Task):
             f.writelines(link + '\n' for link in self.links())
 
 
-def url_is_valid(url):
-    u = urlparse(url)
-    if u.scheme and u.netloc and u.path:
-        return True
-    return False
+class EsocialDownloadSchemaPacks(luigi.Task):
+    def requires(self):
+        return EsocialFetchAvailableSchemaPacks()
+
+    def output(self):
+        return luigi.LocalTarget('downloaded-esocial.yaml')
+
+    def run(self):
+
+        downloaded_files = []
+
+        with self.input().open() as f:
+            available_schema_packs = [i.strip() for i in f.readlines()]
+        for sp in available_schema_packs:
+            downloaded_file = download(sp, dest_dir='downloaded/esocial')
+            downloaded_files.append(dict(
+                url=sp,
+                local_path=downloaded_file,
+                utc_timestamp=datetime.datetime.utcnow().isoformat(),
+            ))
+
+        with self.output().open('w') as fo:
+            fo.write(yaml.dump(downloaded_files, default_flow_style=False))
